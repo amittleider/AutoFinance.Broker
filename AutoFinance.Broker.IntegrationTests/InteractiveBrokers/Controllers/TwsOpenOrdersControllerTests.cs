5acf6abef7f933c0f909d1329d92c6b3084f2708
Andrew Mittleider <amittleider@gmail.com>
2020-03-19 21:09:55 +0100

    Fix open orders

--- AutoFinance.Broker.IntegrationTests/InteractiveBrokers/Controllers/TwsOpenOrdersControllerTests.cs
+++ AutoFinance.Broker.IntegrationTests/InteractiveBrokers/Controllers/TwsOpenOrdersControllerTests.cs
@@ -13,7 +13,7 @@ namespace AutoFinance.Broker.IntegrationTests.InteractiveBrokers.Controllers
     public class TwsOpenOrdersControllerTests
     {
         [Fact]
-        public async Task TwsExecutionController_Should_ReturnExecutions()
+        public async Task TwsExecutionController_Should_ReturnOpenOrders()
         {
             TwsObjectFactory twsObjectFactory = new TwsObjectFactory();
             TwsConnectionController connectionController = new TwsConnectionController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler, "localhost", 7462, 1);
@@ -50,6 +50,48 @@ namespace AutoFinance.Broker.IntegrationTests.InteractiveBrokers.Controllers
 
             var daxOrders = openOrders.Where(orderEvent => orderEvent.Contract.Symbol == "MSFT").ToList();
             daxOrders.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task TwsExecutionController_Should_ReturnOpenOrdersTwice()
+        {
+            TwsObjectFactory twsObjectFactory = new TwsObjectFactory();
+            TwsConnectionController connectionController = new TwsConnectionController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler, "localhost", 7462, 1);
+            TwsOpenOrdersController twsOpenOrdersController = new TwsOpenOrdersController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler);
+            ITwsNextOrderIdController nextOrderIdController = new TwsNextOrderIdController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler);
+            TwsOrderPlacementController orderPlacementController = new TwsOrderPlacementController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler);
+            TwsRequestIdGenerator twsRequestIdGenerator = new TwsRequestIdGenerator();
+            TwsExecutionController executionController = new TwsExecutionController(twsObjectFactory.ClientSocket, twsObjectFactory.TwsCallbackHandler, twsRequestIdGenerator);
+
+            await connectionController.EnsureConnectedAsync();
+
+            // Create a position
+            Contract contract = new Contract();
+            contract.SecType = TwsContractSecType.Stock;
+            contract.Symbol = "MSFT";
+            contract.Exchange = TwsExchange.Smart;
+            contract.Currency = TwsCurrency.Usd;
+            contract.PrimaryExch = TwsExchange.Island;
+
+            Order order = new Order
+            {
+                Action = "BUY",
+                OrderType = "LMT",
+                TotalQuantity = 1,
+                LmtPrice = 1,
+            };
+
+            int orderId = await nextOrderIdController.GetNextValidIdAsync();
+            bool successfullyPlaced = await orderPlacementController.PlaceOrderAsync(orderId, contract, order);
+            Thread.Sleep(1000); // TWS takes some time to put the order in the portfolio. Wait for it.
+
+            var openOrders = await twsOpenOrdersController.RequestOpenOrders();
+            openOrders.Count.Should().BeGreaterOrEqualTo(1);
+
+            Thread.Sleep(5005);
+
+            var openOrders2 = await twsOpenOrdersController.RequestOpenOrders();
+            openOrders2.Count.Should().BeGreaterOrEqualTo(1);
         }
     }
 }
