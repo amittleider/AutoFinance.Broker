fb10a4732f3da8d7dde1b3f9e4f71d86615b20f3
Alexis Côté <alexiscote19@hotmail.com>
2020-06-30 21:03:48 -0400

    Add more information to the error TwsException

--- AutoFinance.Broker/InteractiveBrokers/Exceptions/TwsException.cs
+++ AutoFinance.Broker/InteractiveBrokers/Exceptions/TwsException.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed under the Apache License, Version 2.0.
 
+using AutoFinance.Broker.InteractiveBrokers.EventArgs;
+
 namespace AutoFinance.Broker.InteractiveBrokers.Exceptions
 {
     using System;
@@ -17,5 +19,39 @@ namespace AutoFinance.Broker.InteractiveBrokers.Exceptions
             : base(message)
         {
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TwsException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="errorCode">The TWS error code associated to this exception</param>
+        /// <param name="id">The id associated to this error.</param>
+        public TwsException(string message, int errorCode, int? id)
+            : base(message)
+        {
+            this.ErrorCode = errorCode;
+            this.Id = id;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TwsException"/> class.
+        /// </summary>
+        /// <param name="errorEventArgs">An ErrorEventArgs instance raised by TWS</param>
+        public TwsException(ErrorEventArgs errorEventArgs)
+            : base(errorEventArgs.ErrorMessage)
+        {
+            this.ErrorCode = errorEventArgs.ErrorCode;
+            this.Id = errorEventArgs.Id;
+        }
+
+        /// <summary>
+        /// Gets the error code associated to this exception
+        /// </summary>
+        public int? ErrorCode { get; }
+
+        /// <summary>
+        /// Gets the request id associated to this error.
+        /// </summary>
+        public int? Id { get; }
     }
 }
