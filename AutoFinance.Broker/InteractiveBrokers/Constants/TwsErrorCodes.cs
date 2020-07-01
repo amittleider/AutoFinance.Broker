217cb944808dcb25b65de728b9fd91863b495417
Alexis Côté <alexiscote19@hotmail.com>
2020-07-01 09:00:18 -0400

    Throw exception on place order errors
    - Fail if an order was rejected
    - Add other TwsErrorCodes that could be useful

--- AutoFinance.Broker/InteractiveBrokers/Constants/TwsErrorCodes.cs
+++ AutoFinance.Broker/InteractiveBrokers/Constants/TwsErrorCodes.cs
@@ -8,9 +8,34 @@ namespace AutoFinance.Broker.InteractiveBrokers.Constants
     public class TwsErrorCodes
     {
         /// <summary>
+        /// Ambiguous contract error code
+        /// </summary>
+        public const int AmbiguousContract = 200;
+
+        /// <summary>
+        /// An attempted order was rejected by the IB servers.
+        /// </summary>
+        public const int OrderRejected = 201;
+
+        /// <summary>
         /// OrderCancelled error code
         /// </summary>
         public const int OrderCancelled = 202;
+
+        /// <summary>
+        /// Server error when reading an API client request.
+        /// </summary>
+        public const int ServerErrorWhenReadingApiClientRequest = 320;
+
+        /// <summary>
+        /// Server error when validating an API client request.
+        /// </summary>
+        public const int ServerErrorWhenValidatingApiClientRequest = 321;
+
+        /// <summary>
+        /// Server error when validating an API client request.
+        /// </summary>
+        public const int ServerErrorWhenProcessingApiClientRequest = 322;
 
         /// <summary>
         /// Invalid order type error code
@@ -18,18 +43,18 @@ namespace AutoFinance.Broker.InteractiveBrokers.Constants
         public const int InvalidOrderType = 387;
 
         /// <summary>
-        /// Ambiguous contract error code
+        /// Order message error (i.e Your order was repriced)
         /// </summary>
-        public const int AmbiguousContract = 200;
-
-        /// <summary>
-        /// Order can't be cancelled (possibly filled)
-        /// </summary>
-        public const int OrderCannotBeCancelled = 10148;
+        public const int OrderMessageError = 399;
 
         /// <summary>
         /// Order can't be cancelled (possibly filled)
         /// </summary>
         public const int OrderCannotBeCancelled2 = 10147;
+
+        /// <summary>
+        /// Order can't be cancelled (possibly filled)
+        /// </summary>
+        public const int OrderCannotBeCancelled = 10148;
     }
 }
